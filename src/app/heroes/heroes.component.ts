030b7c79c4fba6f32edba44bc286a063fbdafb38
Damian Nogueiras <damiannogueiras@gmail.com>
2020-01-30 18:59:46 +0100

    Descripción de la clase del componente creado

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+// decorador que especifica los metadatos
 @Component({
+  // nombre del selector CSS del componente, asi se identificará en el html final
   selector: 'app-heroes',
+  // ruta a la plantilla html
   templateUrl: './heroes.component.html',
+  // ruta a la hoja de estilo
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
 
   constructor() { }
-
+  // Angular llamará a esta función después de crear el componente
   ngOnInit() {
   }
 
